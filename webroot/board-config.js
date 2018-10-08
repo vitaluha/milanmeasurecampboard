8eab3830d9eaca7fe9bfef61620eede75ffa2f9a
Vitaliy Matiyash <vitaliy.matiyash@alliancedata.com>
2018-10-08 17:29:56 -0400

    Update board spreadsheet link

--- webroot/board-config.js
+++ webroot/board-config.js
@@ -4,5 +4,6 @@ gOptions = {
   city: 'Milan',
   venue_name: '',
   venue_address: '',
-  google_sheet_url: 'https://docs.google.com/spreadsheets/d/1Axe2cP5tcvJ2XQBnXPd86cEGhzZ_e-02KoMJSN1OV6c/edit?usp=sharing'
+  google_sheet_url: 'https://docs.google.com/spreadsheets/d/13y-ldMXZ9N5ZMFhnuqirQrNnfBUh5Kbx3OLLKHATslM/edit?usp=sharing'
+                     
 }
